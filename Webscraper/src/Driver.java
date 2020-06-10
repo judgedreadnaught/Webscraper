ddf3ffa925fe0d3b4e7e0847183b6f706ac0d34a
judgedreadnaught <akhanal977@gmail.com>
2020-06-10 16:14:43 -0400

    Successfully extracted all the data form the table on yahoo finance for apple. Extracted the prices, but not the label that accompanies the prices.

--- Webscraper/src/Driver.java
+++ Webscraper/src/Driver.java
@@ -1,11 +1,25 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.parser.*;
+import org.jsoup.select.Elements;
 
 public class Driver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
-		System.out.println("commited");
-		System.out.println("Hello");
+		Document website;
+		try {
+			website = Jsoup.connect("https://finance.yahoo.com/quote/AAPL?p=AAPL").get();
+			Elements prices = website.getElementsByClass("Ta(end) Fw(600) lh(14px)"); //
+			//System.out.println(website.getElementById("96"));
+			for (Element x: prices) {
+				System.out.println(x.text());
+			}
+
+		} catch (java.io.IOException e) {
+			System.out.println("Invalid URL Provided");
+		}
 	}
 
 }
