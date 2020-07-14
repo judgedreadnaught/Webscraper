a562a80fcb1647ed427843ffdc6d9a9e945ce8f1
judgedreadnaught <akhanal977@gmail.com>
2020-07-14 12:46:01 -0400

    Extracted the correct labels for the different values of each stock and formatted them correctly for the output

--- Webscraper/src/Driver.java
+++ Webscraper/src/Driver.java
@@ -9,12 +9,24 @@ public class Driver {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Document website;
+		String label = "";
+		String number = "";
 		try {
 			website = Jsoup.connect("https://finance.yahoo.com/quote/AAPL?p=AAPL").get();
 			Elements prices = website.getElementsByClass("Ta(end) Fw(600) lh(14px)"); //
-			//System.out.println(website.getElementById("96"));
-			for (Element x: prices) {
+			Elements names = website.getElementsByClass("C($primaryColor) W(51%)");
+			/*for (Element x: prices) {
 				System.out.println(x.text());
+			}
+			for (Element x: names) {
+				System.out.println(x.text());
+			}*/
+			int counter = 0;
+			while (counter < prices.size()) {
+				label = names.get(counter).text();
+				number = prices.get(counter).text();
+				System.out.println(label.toUpperCase() + ": \t" + number);
+				counter++;
 			}
 
 		} catch (java.io.IOException e) {
